9fd79f1477f9b34e2ad29725a053bf2ad5dd78ce
Joshua Lewis <Darkwater409@gmail.com>
2025-06-18 23:46:28 -0700

    Increase console width and improve echo formatting in Refresh Icon Cache and Restart Explorer scripts

--- Scripts/Refresh Icon Cache.cmd
+++ Scripts/Refresh Icon Cache.cmd
@@ -2,12 +2,12 @@
 
 set BATCH_PATH="res/Refresh Icon Cache.bat"
 
-mode CON COLS=41 LINES=7
+mode CON COLS=45 LINES=7
 color 1F
 title Asking for administrator access
-echo :::::::::::::::::::::::::::::::::::::::::
-echo ::   Requesting administrator access   ::
-echo :::::::::::::::::::::::::::::::::::::::::
+echo :::::::::::::::::::::::::::::::::::::::::::::
+echo ::     Requesting administrator access     ::
+echo :::::::::::::::::::::::::::::::::::::::::::::
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  cmd /u /c echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %Apply%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 color
 cls
@@ -23,17 +23,17 @@ if %errorlevel% == 0 (
 )
 
 
-mode CON COLS=41 LINES=7
+mode CON COLS=45 LINES=7
 color 1F
-echo :::::::::::::::::::::::::::::::::::::::::
-echo ::         Restarting explorer         ::
-echo :::::::::::::::::::::::::::::::::::::::::
+echo :::::::::::::::::::::::::::::::::::::::::::::
+echo ::           Restarting explorer           ::
+echo :::::::::::::::::::::::::::::::::::::::::::::
 echo.
 taskkill /F /IM explorer.exe >nul
 start explorer.exe
-echo :::::::::::::::::::::::::::::::::::::::::
-echo ::               Success               ::
-echo :::::::::::::::::::::::::::::::::::::::::
+echo :::::::::::::::::::::::::::::::::::::::::::::
+echo ::                Success                 ::
+echo :::::::::::::::::::::::::::::::::::::::::::::
 echo.
 echo You can now close this window. It will close automatically in 5 seconds.
 timeout /t 5 >nul
--- Scripts/Restart Explorer.cmd
+++ Scripts/Restart Explorer.cmd
@@ -1,15 +1,16 @@
 @echo off
-mode CON COLS=41 LINES=7
+mode CON COLS=45 LINES=7
 color 1F
 title Applying
-echo :::::::::::::::::::::::::::::::::::::::::
-echo ::         Restarting explorer         ::
-echo :::::::::::::::::::::::::::::::::::::::::
+echo :::::::::::::::::::::::::::::::::::::::::::::
+echo ::           Restarting explorer           ::
+echo :::::::::::::::::::::::::::::::::::::::::::::
+echo.
 taskkill /F /IM explorer.exe >nul
 start explorer.exe
-echo :::::::::::::::::::::::::::::::::::::::::
-echo ::               Success               ::
-echo :::::::::::::::::::::::::::::::::::::::::
+echo :::::::::::::::::::::::::::::::::::::::::::::
+echo ::                Success                 ::
+echo :::::::::::::::::::::::::::::::::::::::::::::
 echo.
 echo You can now close this window. It will close automatically in 5 seconds.
 timeout /t 5 >nul
