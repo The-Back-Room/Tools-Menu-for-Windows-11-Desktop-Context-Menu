de8ed813c2db7fc336dc1f3326e54e1a09388315
Joshua Lewis <Darkwater409@gmail.com>
2025-06-25 09:19:52 -0700

    refactor: simplify Kill Safely Icon script by removing unnecessary process checks

--- Tools/Scripts/Kill Safely Icon.cmd
+++ Tools/Scripts/Kill Safely Icon.cmd
@@ -5,31 +5,11 @@ title Applying
 echo :::::::::::::::::::::::::::::::::::::::::::::
 echo ::          Stopping Safely Icon           ::
 echo :::::::::::::::::::::::::::::::::::::::::::::
-wmic process where "name='safelyicon.exe'" get ProcessID | find /i "ProcessId" > nul
-
-IF ERRORLEVEL 1 (
-	GOTO FAILED
-)
-	
-IF ERRORLEVEL 2 (
-	TASKKILL /F /IM safelyicon.exe >nul
-	GOTO SUCCESS
-)
-
-:SUCCESS
-	echo :::::::::::::::::::::::::::::::::::::::::::::
-	echo ::                Success                  ::
-	echo :::::::::::::::::::::::::::::::::::::::::::::
-	GOTO EXIT
-
-:FAILED
-	echo :::::::::::::::::::::::::::::::::::::::::::::
-	echo ::        No instance(s) detected          ::
-	echo :::::::::::::::::::::::::::::::::::::::::::::
-	GOTO EXIT
-
-:EXIT
-	echo.
-	echo You can now close this window. It will close automatically in 5 seconds.
-	timeout /t 5 >nul
-	EXIT
+taskkill /f /im safelyicon.exe >nul
+echo :::::::::::::::::::::::::::::::::::::::::::::
+echo ::                Success                  ::
+echo :::::::::::::::::::::::::::::::::::::::::::::
+echo.
+echo You can now close this window. It will close automatically in 5 seconds.
+timeout /t 5 >nul
+EXIT
